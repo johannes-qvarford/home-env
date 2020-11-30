7deb4675ddc3c44980bb6555d6c1d8079d3c4b4c
Johannes Qvarford <jq.email+gitlab@pm.me>
2020-11-30 06:20:25 +0100

    Added plexmediaserver

--- BootstrapAdmin1.ps1
+++ BootstrapAdmin1.ps1
@@ -2,7 +2,7 @@
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 choco install -y microsoft-windows-terminal --pre 
-choco install -y dropdown vscode keepass megasync brave steam musicbee plex
+choco install -y dropdown vscode keepass megasync brave steam musicbee plex plexmediaserver
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
