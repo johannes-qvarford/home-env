d99828e2fcfe8315089f616f80207603f7cf5b59
Johannes Qvarford <jq.email+gitlab@pm.me>
2020-12-05 10:44:51 +0100

    Add itch package

--- BootstrapAdmin1.ps1
+++ BootstrapAdmin1.ps1
@@ -2,7 +2,7 @@
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 choco install -y microsoft-windows-terminal --pre 
-choco install -y dropdown vscode keepass megasync brave steam musicbee plex plexmediaserver teamviewer
+choco install -y dropdown vscode keepass megasync brave steam musicbee plex plexmediaserver teamviewer itch
 choco install -y kindlepreviewer --ignore-checksums
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
