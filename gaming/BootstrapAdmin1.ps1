6cbb3089acc1f25e915647782958f9837120137a
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-01-20 21:54:52 +0100

    Change default browser.

--- gaming/BootstrapAdmin1.ps1
+++ gaming/BootstrapAdmin1.ps1
@@ -2,7 +2,7 @@
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 choco install -y microsoft-windows-terminal --pre 
-choco install -y dropbox vscode keepass megasync brave steam plexmediaserver teamviewer itch docker-desktop megatools vlc
+choco install -y dropbox vscode keepass megasync firefox steam plexmediaserver teamviewer itch docker-desktop megatools vlc cpod
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
