a58b3cfd9f5d78c068f9d0a8e41541700f725c2e
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-03-05 19:18:23 +0100

    Wait longer for wsl to start up

--- gaming/autostartup/home-env.bat
+++ gaming/autostartup/home-env.bat
@@ -1,4 +1,4 @@
 %echo off
 echo Waiting a bit before attempting to start wsl script.
-timeout 10
+timeout 600
 C:\WINDOWS\system32\wsl.exe "/home/jq/home-env/gaming/autostartup/home-env.sh"
--- gaming/autostartup/home-env.sh
+++ gaming/autostartup/home-env.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Just to be sure that docker accepts commands, wait a bit.
-sleep 600
-
 docker-compose -f /home/jq/home-env/gaming/win-server/docker-compose.yml up -d
 sleep 5
