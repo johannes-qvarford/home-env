a0a5eb961bd7fb45a4ec30f33ba769231ec25f6e
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-12 22:30:41 +0200

    Replaced packages that no longer install of feedora 34

--- gaming/bootstrap-wsl.sh
+++ gaming/bootstrap-wsl.sh
@@ -3,7 +3,7 @@ sudo dnf install -y git fish
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 sudo dnf check-update
-sudo dnf install -y compat-openssl10 powershell util-linux-user node fzf nmap python3-pip rsync emacs
+sudo dnf install -y powershell util-linux-user nodejs fzf nmap python3-pip rsync emacs
 
 git config user.name 'Johannes Qvarford'
 git config user.email 'jq.email+gitlab@pm.me'
