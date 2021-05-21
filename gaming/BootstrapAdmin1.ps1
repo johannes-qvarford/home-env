9db7dad29aaad97861b655b762e67a4860a4f7ea
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-21 19:51:38 +0200

    Added commands to switch between shells.

--- gaming/BootstrapAdmin1.ps1
+++ gaming/BootstrapAdmin1.ps1
@@ -3,10 +3,14 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 
 choco feature enable -n allowGlobalConfirmation
 choco install microsoft-windows-terminal --pre 
-choco install dropbox vscode keepass megasync firefox brave steam kodi teamviewer itch docker-desktop megatools vlc bleachbit sqlitebrowser stretchly wireshark handbrake fiddler
+choco install dropbox vscode keepass megasync firefox brave steam kodi teamviewer itch docker-desktop megatools vlc bleachbit sqlitebrowser stretchly wireshark handbrake fiddler gsudo microsoft-windows-terminal
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+$modulePath = [Environment]::GetEnvironmentVariable('PSModulePath', 'User')
+$modulePath += ';\\wsl$\fedoraremix\home\johqva\home-env\gaming\powershell'
+[Environment]::SetEnvironmentVariable('PSModulePath', $modulePath, 'User')
 
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Set-Service -Name sshd -StartupType 'Automatic'
