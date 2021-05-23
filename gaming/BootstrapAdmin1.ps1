f3dee144e052d1398b0e025194e11a0800a16992
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-23 20:58:24 +0200

    Autohotkey

--- gaming/BootstrapAdmin1.ps1
+++ gaming/BootstrapAdmin1.ps1
@@ -3,7 +3,7 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 
 choco feature enable -n allowGlobalConfirmation
 choco install microsoft-windows-terminal --pre 
-choco install dropbox vscode keepass megasync firefox brave steam kodi teamviewer itch docker-desktop megatools vlc bleachbit sqlitebrowser stretchly wireshark handbrake fiddler gsudo microsoft-windows-terminal
+choco install dropbox vscode keepass megasync firefox brave steam kodi teamviewer itch docker-desktop megatools vlc bleachbit sqlitebrowser stretchly wireshark handbrake fiddler gsudo microsoft-windows-terminal autohotkey
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
