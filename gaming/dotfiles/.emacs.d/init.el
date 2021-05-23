58b0055d757901def84b481d0cd9965a777b7443
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-23 21:06:37 +0200

    Emacs include smartparens and racket-mode.

---
+++ gaming/dotfiles/.emacs.d/init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives
+	      '("melpa" . "https://melpa.org/packages/")
+	      t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(smartparens racket-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'smartparens-config)
+(add-hook 'racket-mode-hook #'smartparens-mode)
+(add-hook 'racket-mode-hook #'racket-xp-mode)
+(add-hook 'emacs-lisp-mode-hook       #'smartparens-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-mode)
+(add-hook 'ielm-mode-hook             #'smartparens-mode)
+(add-hook 'lisp-mode-hook             #'smartparens-mode)
+(add-hook 'lisp-interaction-mode-hook #'smartparens-mode)
+(add-hook 'scheme-mode-hook           #'smartparens-mode)
