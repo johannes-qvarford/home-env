f58f9d14388443fcb3100b70b99d83c1b5bc9f38
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-05-23 20:58:43 +0200

    WSL and docker startup

--- gaming/autostartup/home-env.sh
+++ gaming/autostartup/home-env.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Make sure that you don't enable automatic Docker Desktop startup - it cannot be restarted with a command from what I know.
+cd "$(wslpath -a "C:\Program Files\Docker\Docker")"
+"./Docker Desktop.exe" &
+sleep 60s
+
 docker-compose -f /home/current/home-env/gaming/win-server/docker-compose.yml up -d
-sleep 5
+sleep 5s
