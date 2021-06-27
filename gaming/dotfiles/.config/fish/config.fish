227fb98d190bc0c08c62f19b7a1cace6003eb2e3
Johannes Qvarford <johqva.email+github@pm.me>
2021-06-27 14:15:55 +0200

    Dotfiles improvements.

--- gaming/dotfiles/.config/fish/config.fish
+++ gaming/dotfiles/.config/fish/config.fish
@@ -3,29 +3,24 @@ set color_repo_paren purple
 set color_repo_branch cyan
 set color_repo_dirty yellow
 
-set -x PYTHONIOENCODING utf-8
-set -x EDITOR "emacs -nw"
-set -x VISUAL "emacs -nw"
-set -x PAGER less
+set -gx PYTHONIOENCODING utf-8
+set -gx EDITOR "emacs -nw"
+set -gx VISUAL "emacs -nw"
+set -gx PAGER less
 
 set -gx PATH $HOME/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin / $PATH 
 set -gx fish_function_path $HOME/.config/fish/functions $fish_function_path
 
 remove_duplicates_in_array PATH
 
-# Force Windows Terminal to use more colors.
-# set -x TERM xterm-color
-
-
-
 set -gx FZF_FIND_FILE_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*(?=config)' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 's@^\./@@'"
 
 set -gx FZF_CD_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*(?=config)' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type d -print 2> /dev/null | sed 's@^\./@@'"
 
 set -gx FZF_CD_WITH_HIDDEN_COMMAND "
@@ -34,7 +29,7 @@ set -gx FZF_CD_WITH_HIDDEN_COMMAND "
     -o -type d -print 2> /dev/null | sed 1d | cut -b3-"
 
 set -gx FZF_OPEN_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*(?=config)' -o -path './winhome/*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | sed 's@^\./@@'"
