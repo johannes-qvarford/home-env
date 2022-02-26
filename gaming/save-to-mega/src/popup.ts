bac92a61ae880d0b601b0a05190e21c693acd048
Johannes Qvarford <johqva.email+github@pm.me>
2022-02-26 20:58:52 +0100

    Dynamic username, password and directory root

---
+++ gaming/save-to-mega/src/popup.ts
@@ -0,0 +1,59 @@
+import { load } from "./load"
+
+function registerField(name: string) {
+    const onChange = (e: Event) => {
+        window.localStorage.setItem(name, (e.target as unknown as any).value)
+    }
+    const email = document.getElementById(name)
+    if (!email || !(email instanceof HTMLInputElement)) {
+        throw new Error("Could not find " + name + " element!")
+    }
+    email.value = localStorage.getItem(name) ?? ""
+    email.addEventListener('change', onChange)
+}
+
+async function init() {
+    console.log("init start")
+    registerField('root')
+    registerField('email')
+    registerField('password')
+    
+
+    const errorElement = document.getElementById("error")
+    if (!errorElement || !(errorElement instanceof HTMLParagraphElement)) {
+        throw new Error("Could not find error element!")
+    }
+    
+    const submit = document.getElementById("submit")
+    if (!submit) {
+        throw new Error("Could not find submit element")
+    }
+    const onSubmit = async (e: Event) => {
+        const email = localStorage.getItem("email") ?? ""
+        const password = localStorage.getItem("password") ?? ""
+        const root = localStorage.getItem("root") ?? ""
+        try {
+            await load(email, password, root, (error) => {
+                //alert(error)
+                errorElement.innerText = error.toString()
+            })
+            errorElement.innerText = "Success!"
+        } catch (error) {
+            console.log("Failed to login with the extension", error)
+            errorElement.innerText = error.toString()
+        }
+        
+    }
+    submit.addEventListener('click', onSubmit)
+}
+
+async function tryInit() {
+    try {
+        console.log("tryInit")
+        await init()
+    } catch (e) {
+        console.log("Failed to load the extension", e)
+    }
+}
+
+tryInit()
