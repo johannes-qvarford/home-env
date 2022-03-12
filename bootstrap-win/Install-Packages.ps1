deef5fe1efbcf52a4969d7e03ca16c20c140aaaa
Johannes Qvarford <jq.email+github@pm.me>
2022-03-12 22:32:27 +0100

    Clarified package ids.

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -1,5 +1,5 @@
-$packages = "Microsoft.WindowsTerminal", "Mozilla Firefox", "Microsoft Visual Studio Code", `
-    "Brave", "Dropbox", "Keepass", "MEGAsync", "Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
+$packages = "Microsoft.WindowsTerminal", "Mozilla.Firefox", "Microsoft Visual Studio Code", `
+    "Brave", "Dropbox", "Keepass", "MEGAsync", "Valve.Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
      "Docker Desktop", "VLC media player", "BleachBit", "DBBrowserForSQLite.DBBrowserForSQLite", `
      "Wireshark", "HandBrake", "Telerik.Fiddler.Classic", "gsudo", "AutoHotkey", "ProtonVPN", `
      "Messenger", "SlackTechnologies.Slack" `
