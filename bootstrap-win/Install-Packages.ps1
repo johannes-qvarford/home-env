5990e2cda0864361e70a7b34942209c4c6ee08dc
Johannes Qvarford <jq.email+github@pm.me>
2022-03-12 22:03:51 +0100

    Use Windows Terminal

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -1,4 +1,4 @@
-$packages = "Windows Terminal Preview", "Mozilla Firefox", "Microsoft Visual Studio Code", `
+$packages = "Microsoft.WindowsTerminal", "Mozilla Firefox", "Microsoft Visual Studio Code", `
     "Brave", "Dropbox", "Keepass", "MEGAsync", "Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
      "Docker Desktop", "VLC media player", "BleachBit", "DBBrowserForSQLite.DBBrowserForSQLite", `
      "Wireshark", "HandBrake", "Telerik.Fiddler.Classic", "gsudo", "AutoHotkey", "ProtonVPN", `
