25578e01e30324287fb4458495ac68593c90bbbf
Johannes Qvarford <johqva.email+github@pm.me>
2022-03-12 15:22:01 +0100

    Moved files out of gaming subfolder.

--- bootstrap-wsl/dotfiles.sh
+++ bootstrap-wsl/dotfiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-new=~
-old="~/home-env/gaming/dotfiles"
+new=$HOME
+old="$HOME/home-env/dotfiles"
 mkdir -p $new/.config
+rm -f $new/.config/fish
 ln -s  $old/.config/fish $new/.config/fish
 
 rm -f $new/.gitconfig
@@ -10,4 +11,5 @@ ln -s $old/.gitconfig $new/.gitconfig
 rm -f $new/.tmux.conf
 ln -s $old/.tmux.conf $new/.tmux.conf
 
+rm -f $new/bin
 ln -s $old/bin $new/bin
