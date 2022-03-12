bff1151d3690664dfbab6a1b3b289a1d35a72d24
Johannes Qvarford <johqva.email+github@pm.me>
2022-03-12 20:10:50 +0100

    Final improvements.

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -1,7 +1,8 @@
 $packages = "Windows Terminal Preview", "Mozilla Firefox", "Microsoft Visual Studio Code", `
     "Brave", "Dropbox", "Keepass", "MEGAsync", "Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
      "Docker Desktop", "VLC media player", "BleachBit", "DBBrowserForSQLite.DBBrowserForSQLite", `
-     "Wireshark", "HandBrake", "Telerik.Fiddler.Classic", "gsudo", "AutoHotkey", "ProtonVPN" `
+     "Wireshark", "HandBrake", "Telerik.Fiddler.Classic", "gsudo", "AutoHotkey", "ProtonVPN", `
+     "Messenger", "SlackTechnologies.Slack" `
 ;
 
 foreach ($package in $packages)
--- bootstrap-wsl/colors.sh
+++ bootstrap-wsl/colors.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-sudo dnf install -y lsd exa git-delta fd-find
+# lsd
+# https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+file=lsd_0.21.0_amd64.deb
+cd /tmp
+wget https://github.com/Peltoche/lsd/releases/download/0.21.0/$file
+sudo dpkg -i /tmp/$file
+sudo apt install -f
+
+file=git-delta_0.11.3_amd64.deb
+cd /tmp
+wget https://github.com/dandavison/delta/releases/download/0.11.3/$file
+sudo dpkg -i /tmp/$file
+sudo apt install -f
+
+# http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-fd-find/fd-find_7.4.0-2build1_amd64.deb
+file=fd-find_7.4.0-2build1_amd64.deb
+cd /tmp
+wget http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-fd-find/$file
+sudo dpkg -i /tmp/$file
+sudo apt install -f
