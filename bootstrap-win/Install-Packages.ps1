4040a8b914b14e7807083539b328b973e429817d
Johannes Qvarford <jq.email+github@pm.me>
2022-05-16 16:41:09 +0200

    7-zip and backup.

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -12,4 +12,7 @@ foreach ($package in $packages)
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# winget is being as I'm writing this, convert these to winget once it settles down.
+choco feature enable -n=allowGlobalConfirmation
 choco install bleachbit
+choco install 7zip
