520cdc5d58228846b241478a3bbe711d6ba716f3
Johannes Qvarford <jq.email+github@pm.me>
2023-03-26 14:09:42 +0200

    Use exact package ids.

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -1,7 +1,7 @@
-$packages = "Microsoft.WindowsTerminal", "Mozilla.Firefox", "Microsoft Visual Studio Code", `
-    "Brave", "Dropbox", "Keepass", "MEGAsync", "Valve.Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
-    "Docker Desktop", "VLC media player", "BleachBit", "DBBrowserForSQLite.DBBrowserForSQLite", `
-    "Wireshark", "HandBrake", "Telerik.Fiddler.Classic", "gsudo", "AutoHotkey", "ProtonVPN", `
+$packages = "Microsoft.WindowsTerminal", "Mozilla.Firefox", "Microsoft.VisualStudioCode", `
+    "Brave.Brave", "Dropbox.Dropbox", "DominikReichl.KeePass", "Mega.MEGASync", "Valve.Steam",  "XBMCFoundation.Kodi", "TeamViewer.TeamViewer", `
+    "VideoLAN.VLC", "DBBrowserForSQLite.DBBrowserForSQLite", `
+    "WiresharkFoundation.Wireshark", "HandBrake.HandBrake", "Telerik.Fiddler.Classic", "gerardog.gsudo", "Lexikos.AutoHotkey", "ProtonTechnologies.ProtonVPN", `
     "Messenger", "SlackTechnologies.Slack", "Stretchly.Stretchly", "Oracle.VirtualBox", "WinDirStat.WinDirStat", `
     "BleachBit.BleachBit", "7zip.7zip", "Microsoft.PowerToys", "Canonical.Ubuntu", `
 ;
