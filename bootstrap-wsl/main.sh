bfb55f3e67d0e8aaee13723487ada2bc38ba2b35
Johannes Qvarford <johannes.qvarford@nexergroup.se>
2023-03-27 10:39:44 +0200

    Add systemd

--- bootstrap-wsl/main.sh
+++ bootstrap-wsl/main.sh
@@ -4,6 +4,7 @@ bash github.sh
 bash clone-home-env.sh
 cd ~/home-env/bootstrap-wsl
 
+bash systemd.sh
 bash docker.sh
 bash dotfiles.sh
 bash fish.sh
---
+++ bootstrap-wsl/systemd.sh
@@ -0,0 +1,4 @@
+sudo cat >>/etc/wsl.conf <<EOF
+[boot]
+systemd=true
+EOF
