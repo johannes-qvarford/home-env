6ab0d566e37785bf1ae5d8dded73b58448b9b29d
Johannes Qvarford <jq.email+github@pm.me>
2023-04-06 23:11:21 +0200

    Better security of server.

--- bootstrap-wsl/github.sh
+++ bootstrap-wsl/github.sh
@@ -4,9 +4,11 @@ sudo apt install -y git
 
 ssh-keygen -t rsa -b 2048 -C "jq.email+github@pm.me"
 echo
-echo paste the below into https://github.com/settings/keys
+echo paste the below into https://github.com/settings/keys and https://cloud.digitalocean.com/account/security
 echo
 cat ~/.ssh/id_rsa.pub
 echo
 read -p "Have you copied the above? "
 echo
+echo now a passphrase will be requested.
+ssh-keygen -p -o -f ~/.ssh/id_rsa
