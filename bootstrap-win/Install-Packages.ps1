2ee36338066cda9108c0b592e1b04c94fc849d35
Johannes Qvarford <jq.email+github@pm.me>
2023-08-29 22:59:38 +0200

    logseq

--- bootstrap-win/Install-Packages.ps1
+++ bootstrap-win/Install-Packages.ps1
@@ -10,7 +10,7 @@ foreach ($package in $packages)
   winget install $package
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco feature enable -n=allowGlobalConfirmation
 choco install tartube
