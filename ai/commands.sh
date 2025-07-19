daea55c99c03f9bce0e50d263c6e10e531e12e43
Johannes Qvarford <jq.email+github@pm.me>
2025-07-19 19:38:21 +0200

    Remove Open WebUI and friends.

--- ai/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for model in "$1/"*.Modelfile; do
-    model_name=$(basename "$model" .Modelfile)
-    echo "Creating model: $model_name"
-    ollama create $model_name -f "$1/$model_name.Modelfile"
-done
