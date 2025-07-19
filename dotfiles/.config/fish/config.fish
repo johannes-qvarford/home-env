3350b8e2f148486e45b697744b94bb8dd86efb8f
Johannes Qvarford <jq.email+github@pm.me>
2025-07-19 17:24:19 +0200

    Stuff

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -8,7 +8,8 @@ set -gx EDITOR "code"
 set -gx VISUAL "code"
 set -gx PAGER less
 set -gx PULSE_SERVER unix:/mnt/wslg/PulseServer
-set -gx OPENAI_API_KEY sk-af18da76518d4847bac595061c86109c
+set -x OPENROUTER_KEY (cat ~/.openrouter_key)
+set -gx OPENAI_API_KEY $OPENROUTER_KEY
 
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
@@ -17,3 +18,4 @@ fish_add_path $HOME/bin
 fish_add_path $HOME/projects/vcpkg
 set -x CMAKE_TOOLCHAIN_FILE $HOME/projects/vcpkg/scripts/buildsystems/vcpkg.cmake
 alias claude="/home/jq/.claude/local/claude"
+
