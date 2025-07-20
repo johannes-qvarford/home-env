1f326f932e125c23e8f0bcac854f1327fdfb7937
Johannes Qvarford <jq.email+github@pm.me>
2025-07-20 19:41:39 +0200

    Claude Code Router and librechat changes.

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -11,10 +11,11 @@ set -gx PULSE_SERVER unix:/mnt/wslg/PulseServer
 set -x OPENROUTER_KEY (cat ~/.openrouter_key)
 set -gx OPENAI_API_KEY $OPENROUTER_KEY
 
+fish_add_path $HOME/bin
+fish_add_path $HOME/.npm/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/JetBrains/Toolbox/scripts
-fish_add_path $HOME/bin
 fish_add_path $HOME/projects/vcpkg
+
 set -x CMAKE_TOOLCHAIN_FILE $HOME/projects/vcpkg/scripts/buildsystems/vcpkg.cmake
-alias claude="/home/jq/.claude/local/claude"
